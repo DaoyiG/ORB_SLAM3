44110ef30560f2eb89d0d1d2740baa4a1e034a20
DaoyiG <seraphgao2014@hotmail.com>
2020-12-16 09:12:12 -0800

    results mp now is for rgb input

--- kitti_examples.sh
+++ kitti_examples.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-pathDatasetKitti='/home/daoyig/ORB_SLAM3/Dataset_Test/Kitti/sequences'
+pathDatasetKitti='/home/daoyig/ORB_SLAM3/Dataset/Kitti/sequences'
 pathOutput='/home/daoyig/ORB_SLAM3'
 #echo "Launching Kitti00 with Stereo sensor"
 #./Examples/Stereo/stereo_kitti ./Vocabulary/ORBvoc.txt ./Examples/Stereo/KITTI00-02.yaml "$pathDatasetKitti"/00
 
 echo "Launching Kitti0 with Stereo sensor (only has 20 frames)"
-./Examples/Stereo/stereo_kitti ./Vocabulary/ORBvoc.txt ./Examples/Stereo/KITTI00-02.yaml "$pathDatasetKitti"/0 "$pathOutput"/
+./Examples/Stereo/stereo_kitti ./Vocabulary/ORBvoc.txt ./Examples/Stereo/KITTI00-02.yaml "$pathDatasetKitti"/00 "$pathOutput"/

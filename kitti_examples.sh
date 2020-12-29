d09704a0cac59d9c1872e92394caf20dcf960e20
DaoyiG <seraphgao2014@hotmail.com>
2020-12-29 05:15:54 -0800

    fix K

--- kitti_examples.sh
+++ kitti_examples.sh
@@ -5,9 +5,9 @@ pathOutput='/home/daoyig/ORB_SLAM3'
 pathDatasetKittiresized='/home/daoyig/ORB_SLAM3/Dataset/kitti_odom_resized/sequences'
 
 #echo "Launching Kitti00 with Stereo sensor"
-#./Examples/Stereo/stereo_kitti ./Vocabulary/ORBvoc.txt ./Examples/Stereo/KITTI00-02.yaml "$pathDatasetKitti"/00
+#./Examples/Stereo/stereo_kitti ./Vocabulary/ORBvoc.txt ./Examples/Stereo/KITTI00-02.yaml #"$pathDatasetKitti"/00
 
-#echo "Launching Kitti0 with Stereo sensor (only has 20 frames)"
+#echo "Launching Kitti0 with Stereo sensor"
 #./Examples/Stereo/stereo_kitti ./Vocabulary/ORBvoc.txt ./Examples/Stereo/KITTI00-02.yaml "$pathDatasetKitti"/00 "$pathOutput"/
 
 echo "Launching Kitti0 with Stereo sensor for 640 x 192"
